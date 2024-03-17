5ac65bd08b8e6932fdcfa7a6892d3c85e317140d
Sparsh-R17 <sparsh170702@gmail.com>
2024-03-17 11:49:47 +0530

    Added Custom AppBar

--- app/src/main/java/com/jetapp/jet_weather/screens/main/MainScreen.kt
+++ app/src/main/java/com/jetapp/jet_weather/screens/main/MainScreen.kt
@@ -1,34 +1,72 @@
 package com.jetapp.jet_weather.screens.main
 
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.jetapp.jet_weather.data.DataOrException
 import com.jetapp.jet_weather.model.Weather
+import com.jetapp.jet_weather.widgets.WeatherAppBar
 
 @Composable
 fun MainScreen(
     navController: NavController,
     mainViewModel: MainViewModel = hiltViewModel()
 ) {
-    ShowData(mainViewModel)
-}
-
-@Composable
-fun ShowData(mainViewModel: MainViewModel) {
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)
     ) {
-        value = mainViewModel.getWeatherData(city = "Kaushambi")
+        value = mainViewModel.getWeatherData(city = "Ghaziabad")
     }.value
 
     if(weatherData.loading == true){
         CircularProgressIndicator()
     }else if(weatherData.data != null){
-        Text(text = weatherData.data.toString())
+        MainScaffold(
+            weather = weatherData.data!!,
+            navController = navController
+        )
     }
+    
+}
 
+@Composable
+fun MainScaffold(
+    weather: Weather,
+    navController: NavController) {
+    Scaffold(
+        topBar = {
+            WeatherAppBar(
+                title = weather.city.name + ", ${weather.city.country}",
+                navController = navController,
+                elevation = 2.dp,
+            ){
+                Log.d("TAG", "MainScaffold: ButtonCLicked")
+            }
+        }
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            MainContent(data  = weather)
+        }
+    }
+}
+
+
+
+@Composable
+fun MainContent(data: Weather) {
+    Text(text = data.city.name)
 }
---
+++ app/src/main/java/com/jetapp/jet_weather/widgets/WeatherAppBar.kt
@@ -0,0 +1,85 @@
+package com.jetapp.jet_weather.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun WeatherAppBar(
+    title: String = "TITLE",
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController? = null,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {},
+) {
+    Surface(
+        shadowElevation = elevation
+    ) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = title,
+                    color = MaterialTheme.colorScheme.primary,
+                    style = TextStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 17.sp,
+                    )
+                )
+            },
+            actions = {
+                if (isMainScreen) {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(Icons.Default.Search,contentDescription = "Search Icon")
+                    }
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Default.MoreVert, contentDescription = "More Option")
+                    }
+                }else{
+                    Box {
+
+                    }
+                }
+            },
+            navigationIcon = {
+                if (icon != null){
+                    Icon(
+                        modifier = Modifier.clickable {
+                            onButtonClicked.invoke()  //act as a callback
+                        },
+                        imageVector = icon,
+                        contentDescription = "Navigation Icon",
+                    )
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.inversePrimary
+            ),
+        )
+    }
+}
